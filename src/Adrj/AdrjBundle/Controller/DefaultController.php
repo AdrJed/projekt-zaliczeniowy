dd224b4dcec9ad7289f863d43b02b80f02ca5b40
Adrian <adrianj@windowslive.com>
2014-01-25 13:39:01 +0100

    Dodano wstępną autoryzację

--- src/Adrj/AdrjBundle/Controller/DefaultController.php
+++ src/Adrj/AdrjBundle/Controller/DefaultController.php
@@ -3,12 +3,34 @@
 namespace Adrj\AdrjBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-    // Akcja zrwacająca widok strony głównej
+    // Akcja zwracająca widok strony głównej
         return $this->render('AdrjBundle:Default:index.html.twig');
+    }
+
+    public function loginAction(Request $request)
+    {
+    // Akcja zwracająca widok formularza do logowania
+        $session = $request->getSession();
+
+        if ( $request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+        {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        else
+        {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('AdrjBundle:Security:login.html.twig', array(
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME), 
+            'error' => $error));
     }
 }
