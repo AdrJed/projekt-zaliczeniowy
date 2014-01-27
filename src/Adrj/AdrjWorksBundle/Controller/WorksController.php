52d6d4e4ae9c02ca6f12c24294d621c484033a0a
Adrian <adrianj@windowslive.com>
2014-01-27 22:50:05 +0100

    Dodanie widoków i modyfikacja kontrolera WorksController w AdrjWorksBundle

--- src/Adrj/AdrjWorksBundle/Controller/WorksController.php
+++ src/Adrj/AdrjWorksBundle/Controller/WorksController.php
@@ -16,13 +16,17 @@ class WorksController extends Controller
     public function listAction($category)
     {
     // Akcja listująca prace z danej kategorii (graphics, programs, websites, all)
+        $entityManager = $this->getDoctrine()->getManager();
+        $worksList = $entityManager->getRepository('AdrjWorksBundle:WorksList');
+
         if ($category == 'all')
         {
-            
+            $worksList = $worksList->findAll();
         }
         else
         {
-            
+            // Zwraca listę prac dla wybranej kategorii
+            // Prawdopodobnie użycie JOIN dla WorksCategories i WorksList
         }
 
         return $this->render('AdrjWorksBundle:Works:list.html.twig');
@@ -31,22 +35,41 @@ class WorksController extends Controller
     public function showAction($category,$id_name)
     {
     // Akcja zwracający widok szczegółów dla elementu z podanej kategorii i wybranym id-name
+        if ( $category == 'graphics' )
+        {
+            return $this->graphicsAction($id_name);
+        }
+        elseif ( $category == 'programs')
+        {
+            return $this->programAction($id_name);
+        }
+        elseif ( $category == 'websites' )
+        {
+            return $this->websiteAction($id_name);
+        }
         
-        return $this->render('AdrjWorksBundle:Works:show.html.twig');
+        //return $this->render('AdrjWorksBundle:Works:show.html.twig');
     }
 
     public function programAction($id_name)
     {
-    // Akcja zwracająca widok dla szczegółów programu
-        
+    // Akcja zwracająca widok dla szczegółów programu       
+
+        return $this->render('AdrjWorksBundle:Works:show.program.html.twig');
     }
 
     public function graphicsAction($id_name)
     {
     // Akcja zwracająca widok dla szczegółów grafiki
-        
+
+        return $this->render('AdrjWorksBundle:Works:show.graphics.html.twig');    
     }
 
-    public function 
+    public function websiteAction($id_name)
+    {
+    // Akcja zwracająca widok dla szczegółów strony głównej
+
+        return $this->render('AdrjWorksBundle:Works:show.website.html.twig');
+    }
 
 }
